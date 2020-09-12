d1b0070a8dcabf7948adfec470a94a5160bb405d
Julian <julian@jgroen.com>
2020-09-12 15:23:29 -0600

    new grid method

--- src/components/GameBoardCanvas.js
+++ src/components/GameBoardCanvas.js
@@ -3,62 +3,32 @@ import React, { useState, useEffect, useRef } from 'react';
 const GameBoardCanvas = () => {
     const canvasEl = useRef(null);
     const cellSize = 16;
+
     const [dimensions, setDimensions] = useState({
         height: window.innerHeight,
         width: window.innerWidth,
     });
-    const [cellArray, setCellArray] = useState(createGrid(dimensions.height, dimensions.width, cellSize));
 
     function handleResize() {
         setDimensions({
             height: window.innerHeight,
             width: window.innerWidth,
         });
-        setCellArray(createGrid(dimensions.height, dimensions.width, cellSize));
     }
 
-    function setCanvas() {
-        const canvas = canvasEl.current;
-        const ctx = canvas.getContext('2d');
-        canvas.height = dimensions.height;
-        canvas.width = dimensions.width;
-        //ctx.fillStyle = 'black';
-        //ctx.fillRect(0, 0, canvas.width, canvas.height);
-        //setCellArray(createGrid(canvas.height, canvas.width));
-        cellArray.forEach((row, rowIndex) => {
-            row.forEach((cell, cellIndex) => {
-                const x = cellIndex * cellSize;
-                const y = rowIndex * cellSize;
-                if (cell === '1') {
-                    ctx.fillStyle = 'slateblue';
-                } else {
-                    ctx.fillStyle = 'black';
-                }
-                ctx.fillRect(x, y, cellSize, cellSize);
-            });
-        });
-    }
-
-    // create a grid that fits the viewport
-    function createGrid(height, width, size) {
-        const cols = Math.floor(width / size);
-        const rows = Math.floor(height / size);
+    function createGrid(height, width, cellSize) {
         const array = [];
-        for (let i = 0; i < rows; i++) {
+        while ((array.length + 1) * cellSize <= height) {
             const row = [];
-            for (let j = 0; j < cols; j++) {
-                if (Math.floor(Math.random() * 2) === 0) {
-                    row.push('0');
-                } else {
-                    row.push('1');
-                }
+            while ((row.length + 1) * cellSize <= width) {
+                row.push('0');
             }
             array.push(row);
         }
         return array;
     }
 
-    function cellularAutomata() {
+    function cellularAutomata(cellArray) {
         //console.log('start', cellArray);
         const array = cellArray.map((row, rowIndex) => {
             return row.map((cell, cellIndex) => {
@@ -73,13 +43,10 @@ const GameBoardCanvas = () => {
                     } else if (aliveNeighbors.length === 2 || aliveNeighbors.length === 3) {
                         return '1';
                     }
-                } else {
-                    if (aliveNeighbors.length === 3) {
-                        return '1';
-                    } else {
-                        return '0';
-                    }
+                } else if (aliveNeighbors.length === 3) {
+                    return '1';
                 }
+                return '0';
             });
         });
         //console.log('finish', array);
@@ -117,7 +84,6 @@ const GameBoardCanvas = () => {
 
     useEffect(() => {
         window.addEventListener('resize', handleResize);
-        setCanvas();
 
         return () => {
             window.removeEventListener('resize', handleResize);
@@ -125,15 +91,14 @@ const GameBoardCanvas = () => {
     });
 
     return (
-        <div className="relative">
-            <button
-                className="absolute text-2xl text-indigo-400 bg-gray-800 p-4"
-                //onClick={() => setCellArray(createGrid(dimensions.height, dimensions.width, cellSize))}
-                onClick={() => setCellArray(cellularAutomata())}
-            >
-                Test
-            </button>
-            <canvas ref={canvasEl}></canvas>
+        <div className="bg-black relative">
+            {createGrid(dimensions.height, dimensions.width, cellSize).map((row, index) => {
+                return row.map((cell, index) => {
+                    return <div className="bg-indigo-800 text-gray-200 p-4 m-4">{cell}</div>;
+                });
+            })}
+            <button className="absolute text-2xl text-indigo-400 bg-gray-800 p-4">Width: {dimensions.width}</button>
+            <canvas ref={canvasEl} width={dimensions.width} height={dimensions.height}></canvas>
         </div>
     );
 };
