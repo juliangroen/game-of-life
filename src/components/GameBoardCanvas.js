4254ec79ed7764b3d0e6b3eb3b5113ee329d4446
Julian <julian@jgroen.com>
2020-09-12 19:10:54 -0600

    WIP cellular automata

--- src/components/GameBoardCanvas.js
+++ src/components/GameBoardCanvas.js
@@ -8,44 +8,78 @@ const GameBoardCanvas = () => {
         height: window.innerHeight,
         width: window.innerWidth,
     });
+    const [grid, setGrid] = useState(createGrid());
 
     function handleResize() {
         setDimensions({
             height: window.innerHeight,
             width: window.innerWidth,
         });
+        setGrid(createGrid());
     }
 
-    function createGrid(height, width, cellSize) {
+    function createGrid(randomize = false, height = dimensions.height, width = dimensions.width, cSize = cellSize) {
         const array = [];
-        while ((array.length + 1) * cellSize <= height) {
+        while ((array.length + 1) * cSize <= height) {
             const row = [];
-            while ((row.length + 1) * cellSize <= width) {
-                row.push('0');
+            while ((row.length + 1) * cSize <= width) {
+                if (randomize) {
+                    const rand = Math.floor(Math.random() * 2);
+                    if (rand === 0) {
+                        row.push('0');
+                    } else {
+                        row.push('1');
+                    }
+                } else {
+                    row.push('0');
+                }
             }
             array.push(row);
         }
         return array;
     }
 
+    function drawGrid(array, canvas, cellSize) {
+        const ctx = canvas.current.getContext('2d');
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        ctx.beginPath();
+        array.forEach((row, rowIndex) => {
+            row.forEach((cell, cellIndex) => {
+                const x = cellIndex * cellSize;
+                const y = rowIndex * cellSize;
+                if (cell === '1') {
+                    ctx.fillStyle = 'slateblue';
+                    ctx.fillRect(x, y, cellSize, cellSize);
+                }
+            });
+        });
+    }
+
     function cellularAutomata(cellArray) {
         //console.log('start', cellArray);
         const array = cellArray.map((row, rowIndex) => {
             return row.map((cell, cellIndex) => {
-                const aliveNeighbors = findNeighbors(rowIndex, cellIndex, cellArray).filter((n) => {
+                //const aliveNeighbors = findNeighbors(rowIndex, cellIndex, cellArray).filter((n) => {
+                //    const row = n[0];
+                //    const cell = n[1];
+                //    return cellArray[row][cell] === '1' && n;
+                //});
+                //console.log(cellArray[rowIndex][cellIndex]);
+                const aliveNeighbors = findNeighbors(rowIndex, cellIndex, cellArray);
+                const test = aliveNeighbors.map((n) => {
                     const row = n[0];
                     const cell = n[1];
-                    return cellArray[row][cell] === '1' && n;
+                    console.log(cellArray[row][cell]);
                 });
-                if (cell === '1') {
-                    if (aliveNeighbors.length < 2 || aliveNeighbors.length > 3) {
-                        return '0';
-                    } else if (aliveNeighbors.length === 2 || aliveNeighbors.length === 3) {
-                        return '1';
-                    }
-                } else if (aliveNeighbors.length === 3) {
-                    return '1';
-                }
+                //if (cell === '1') {
+                //    if (aliveNeighbors.length < 2 || aliveNeighbors.length > 3) {
+                //        return '0';
+                //    } else if (aliveNeighbors.length === 2 || aliveNeighbors.length === 3) {
+                //        return '1';
+                //    }
+                //} else if (aliveNeighbors.length === 3) {
+                //    return '1';
+                //}
                 return '0';
             });
         });
@@ -84,6 +118,7 @@ const GameBoardCanvas = () => {
 
     useEffect(() => {
         window.addEventListener('resize', handleResize);
+        drawGrid(grid, canvasEl, cellSize);
 
         return () => {
             window.removeEventListener('resize', handleResize);
@@ -92,12 +127,12 @@ const GameBoardCanvas = () => {
 
     return (
         <div className="bg-black relative">
-            {createGrid(dimensions.height, dimensions.width, cellSize).map((row, index) => {
-                return row.map((cell, index) => {
-                    return <div className="bg-indigo-800 text-gray-200 p-4 m-4">{cell}</div>;
-                });
-            })}
-            <button className="absolute text-2xl text-indigo-400 bg-gray-800 p-4">Width: {dimensions.width}</button>
+            <button className="absolute top-0 left-0 text-indigo-400 bg-gray-800 p-4" onClick={() => setGrid(createGrid(true))}>
+                Grid: {grid.length} x {grid[0].length}
+            </button>
+            <button className="absolute top-0 right-0 text-indigo-400 bg-gray-800 p-4" onClick={() => setGrid(cellularAutomata(grid))}>
+                Automata
+            </button>
             <canvas ref={canvasEl} width={dimensions.width} height={dimensions.height}></canvas>
         </div>
     );
